4f77de03b2604c4094a09c700a14c8e9fec737e8
Candidate <candidate@shauls-macbook-pro-2.local>
2016-09-07 11:54:36 +0530

    Initial Commit

---
+++ login_github/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  login_github
+//
+//  Created by Candidate on 9/7/16.
+//  Copyright © 2016 Candidate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ login_github/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  login_github
+//
+//  Created by Candidate on 9/7/16.
+//  Copyright © 2016 Candidate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
